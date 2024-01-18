8d28eba6003d6c09d588f2f56f9bb52ca6b7e57e
JSH61 <j.schuler@alsto.de>
2024-01-18 21:43:20 +0100

    Any Test changes

--- first-app.js
+++ first-app.js
@@ -5,4 +5,19 @@
     console.log('Hello Node.JS');
 
     // This is a new Line to Test GitHub
-    
+       // Add the FileSystem Module from node.js
+    //const fs = require ('fs');
+
+    //fs.writeFileSync('')
+    console.log('Hello Node.JS');
+
+const http = require('http');
+const port = process.env.PORT || 7777;
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end("Hello, World! JSH Port: $port\n");
+}).listen(port);
+
+console.log(`App is running... (port: ${port})`);
+
